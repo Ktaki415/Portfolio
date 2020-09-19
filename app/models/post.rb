8cb25c507ed3033fd5adb23665ba4f8b03c93762
vagrant <k.taki415@gmail.com>
2020-09-19 08:03:56 +0000

    [Finish] #16

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+
+	acts_as_taggable_on :tags
 	belongs_to :user
 	has_many :favorites, dependent: :destroy
 	has_many :post_comments, dependent: :destroy
@@ -11,4 +13,6 @@ class Post < ApplicationRecord
 	def favorited_by?(user)
 	  favorites.where(user_id: user.id).exists?
 	end
+
+	
 end
